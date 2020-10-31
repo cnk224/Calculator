089fc2e863c473eb376cee01be78efe3c0094d40
cnk224 <oumartoure405@gmail.com>
2020-10-31 23:49:39 +0100

    Intermediare updates

--- src/view/MainCalculatriceFxml.java
+++ src/view/MainCalculatriceFxml.java
@@ -32,7 +32,7 @@ public class MainCalculatriceFxml implements Initializable{
 	@FXML
 	private Button button9;
 	@FXML
-	private Button percentageButton;
+	private Button multipleButton;
 	@FXML
 	private Button soustractButton;
 	@FXML
@@ -47,8 +47,9 @@ public class MainCalculatriceFxml implements Initializable{
 	@FXML
 	private TextField display;
 	
-	@FXML
-	private Button[] buttonTab = new Button[15];
+	private int operate = -1;
+	private float result;
+	private float firstNumber;
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
@@ -58,13 +59,98 @@ public class MainCalculatriceFxml implements Initializable{
 	
 	@FXML
 	void handleActionButton(ActionEvent event) {
-		System.out.println(event.getSource());
+		
+		if(event.getSource() == button0) {
+			display.setText(display.getText() + "0");
+		}else
 		if(event.getSource() == button1) {
-			System.out.println("Bouton 1");
+			display.setText(display.getText() + "1");
+		}else
+		
+		if(event.getSource() == button2) {
+			display.setText(display.getText() + "2");
+		}else
+		
+		if(event.getSource() == button3) {
+			display.setText(display.getText() + "3");
+		}else
+		
+		if(event.getSource() == button4) {
+			display.setText(display.getText() + "4");
+		}else
+		
+		if(event.getSource() == button5) {
+			display.setText(display.getText() + "5");
+		}else
+		
+		if(event.getSource() == button6) {
+			display.setText(display.getText() + "6");
+		}else
+		
+		if(event.getSource() == button7) {
+			display.setText(display.getText() + "7");
+		}else
+		
+		if(event.getSource() == button8) {
+			display.setText(display.getText() + "8");
+		}else
+		
+		if(event.getSource() == button9) {
+			display.setText(display.getText() + "9");
+		}else
+		
+		if(event.getSource() == addButton) {
+			firstNumber = Float.parseFloat(display.getText());
+			operate = 1;
+			display.setText("");
+		}else
+		
+		if(event.getSource() == soustractButton) {
+			firstNumber = Float.parseFloat(display.getText());
+			operate = 2;
+			display.setText("");
+			
+		}else
+		
+		if(event.getSource() == multipleButton) {
+			firstNumber = Float.parseFloat(display.getText());
+			operate = 3;
+			display.setText("");
+		}else
+		
+		if(event.getSource() == divideButton) {
+			firstNumber = Float.parseFloat(display.getText());
+			operate = 4;
+			display.setText("");
+		}else if(event.getSource() == equalButton) {
+			float secondNumber = Float.parseFloat(display.getText());
+			
+			switch(operate) {
+			case 1:
+				result = firstNumber + secondNumber;
+				display.setText(String.valueOf(result));
+				System.out.println("firstNumber: "+firstNumber);
+				break;
+			case 2:
+				result = firstNumber + secondNumber;
+				display.setText(String.valueOf(result));
+				System.out.println("firstNumber: "+firstNumber);
+				break;
+			case 3:
+				result = firstNumber * secondNumber;
+				display.setText(String.valueOf(result));
+				System.out.println("firstNumber: "+firstNumber);
+				break;
+			case 4:
+				result = firstNumber / secondNumber;
+				display.setText(String.valueOf(result));
+				System.out.println("firstNumber: "+firstNumber);
+				break;
+			}
+			
+			
 		}
 		
 	}
-	
-	
 	
 }
